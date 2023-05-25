d0675fff47c6e3ba94c74f7e402e693478ef25a4
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-25 23:50:55 +0200

    GV5-4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Permission::create(['name' => 'edit articles']);
+      //  Permission::create(['name' => 'delete articles']);
+      //  Permission::create(['name' => 'publish articles']);
+      //  Permission::create(['name' => 'unpublish articles']);
+
+      //  // create roles and assign created permissions
+
+      //  // this can be done as separate statements
+      //  $role = Role::create(['name' => 'writer']);
+      //  $role->givePermissionTo('edit articles');
+
+      //  // or may be done by chaining
+      //  $role = Role::create(['name' => 'moderator'])
+      //      ->givePermissionTo(['publish articles', 'unpublish articles']);
+
+      //  $role = Role::create(['name' => 'super-admin']);
+      //  $role->givePermissionTo(Permission::all());
     }
 }
