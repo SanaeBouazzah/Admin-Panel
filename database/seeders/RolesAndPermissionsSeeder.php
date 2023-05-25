e7930d6843b051024eaa5a47d4ef01dc9ebd3a74
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-25 23:47:42 +0200

    GV5-4

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -12,6 +12,6 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+      app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
     }
 }
