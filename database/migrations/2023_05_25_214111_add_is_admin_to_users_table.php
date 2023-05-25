51037d96315b9c3e71afafaa43620dea23e2c982
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-25 23:44:08 +0200

    GV5-4

--- database/migrations/2023_05_25_214111_add_is_admin_to_users_table.php
+++ database/migrations/2023_05_25_214111_add_is_admin_to_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn()
+            $table->dropColumn('is_admin');
         });
     }
 };
