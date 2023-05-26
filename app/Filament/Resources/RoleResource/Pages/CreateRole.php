bbb4378553542da7315c3e1faa861f49010751f3
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-26 23:37:27 +0200

    GV5-4

--- app/Filament/Resources/RoleResource/Pages/CreateRole.php
+++ app/Filament/Resources/RoleResource/Pages/CreateRole.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRole extends CreateRecord
 {
     protected static string $resource = RoleResource::class;
+    protected function getRedirectUrl(): string
+    {
+      
+    }
 }
