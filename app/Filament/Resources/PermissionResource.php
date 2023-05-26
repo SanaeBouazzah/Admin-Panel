7e4cb0236c5aef2eb0011925387000971dbfcf3d
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-26 23:08:04 +0200

    GV5-4

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -20,7 +20,9 @@ class PermissionResource extends Resource
 {
     protected static ?string $model = Permission::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-key';
+
+     d
 
     public static function form(Form $form): Form
     {
