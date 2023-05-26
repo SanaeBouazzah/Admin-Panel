be413431c87cd9320f89f6068330f5fa02cdfaa1
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-26 22:52:53 +0200

    GV5-4

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -26,7 +26,7 @@ class PermissionResource extends Resource
             ->schema([
                 TextColumn::make('id')->sortable(),
                 TextColumn::make('name')->sortable()->searchable(),
-                TextColumn::make('created_at')->sortable()->searchable(),
+                TextColumn::make('created_at')->date()->sortable()->searchable(),
             ]);
     }
 
