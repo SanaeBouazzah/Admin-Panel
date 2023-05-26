d57f9af065c1f47dc4d7a8c631b4eead63dcdc90
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-26 23:38:29 +0200

    GV5-4

--- app/Filament/Resources/RoleResource/Pages/CreateRole.php
+++ app/Filament/Resources/RoleResource/Pages/CreateRole.php
@@ -11,6 +11,6 @@ class CreateRole extends CreateRecord
     protected static string $resource = RoleResource::class;
     protected function getRedirectUrl(): string
     {
-      
+      return $this->getResource()::getUrl('index');
     }
 }
