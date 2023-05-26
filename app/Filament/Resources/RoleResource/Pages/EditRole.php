7fa043ed6634d89ee48aa6ca11174693f7d6f7e5
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-26 23:39:25 +0200

    GV5-4

--- app/Filament/Resources/RoleResource/Pages/EditRole.php
+++ app/Filament/Resources/RoleResource/Pages/EditRole.php
@@ -16,4 +16,8 @@ class EditRole extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+    protected function getRedirectUrl(): string
+    {
+      return $this->getResource()::getUrl('index');
+    }
 }
