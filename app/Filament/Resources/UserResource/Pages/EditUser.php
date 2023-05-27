d0c5e122f38b3d8b6763561125a82faad585336f
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-27 20:31:43 +0200

    GV5-4

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -16,4 +16,8 @@ class EditUser extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+    protected function getRedirectUrl(): string
+    {
+      return $this->getResource()::getUrl('index');
+    }
 }
