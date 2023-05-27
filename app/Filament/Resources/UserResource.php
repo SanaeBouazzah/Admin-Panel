4695430b413eee623ceca7449154d2433c11c4e9
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-27 20:20:44 +0200

    GV5-4

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -32,7 +32,6 @@ class UserResource extends Resource
                     ->email()
                     ->required()
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->required()
