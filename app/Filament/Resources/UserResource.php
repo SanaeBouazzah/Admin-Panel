553f157f37af9c518f5f4a1896d376af2ecb6172
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-27 20:08:25 +0200

    GV5-4

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -47,8 +47,8 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('name'),
                 Tables\Columns\IconColumn::make('is_admin')->sortable()->searchable()->boolean(),
                 Tables\Columns\TextColumn::make('email')->sortable()->searchable(),
-                Tables\Columns\TextColumn::make('email')->sortable()->searchable(),
-                Tables\Columns\TextColumn::make('created_at')->dateTime('d-M-YGV5-4')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->dateTime('d-M-Y')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->dateTime('d-M-Y')->sortable()->searchable(),
             ])
             ->filters([
                 //
