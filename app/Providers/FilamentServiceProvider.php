46d9c9886e04e3b3b7391cae9c70f18edcba87d4
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-28 21:43:34 +0200

    GV5-4

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -19,6 +19,28 @@ class FilamentServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+      Filament::serving(function() {
+        Filament::registerTheme(
+            app(Vite::class)('resources/css/filament.css'),
+        );
+        if (auth()->user()) {
+            if (auth()->user()->is_admin === 1 && auth()->user()->hasAnyRole(['super-admin', 'admin', 'moderator'])) {
+                Filament::registerUserMenuItems([
+                    UserMenuItem::make()
+                        ->label('Manage Users')
+                        ->url(UserResource::getUrl())
+                        ->icon('heroicon-s-users'),
+                    UserMenuItem::make()
+                        ->label('Manage Roles')
+                        ->url(RoleResource::getUrl())
+                        ->icon('heroicon-s-cog'),
+                    UserMenuItem::make()
+                        ->label('Manage Permissions')
+                        ->url(PermissionResource::getUrl())
+                        ->icon('heroicon-s-key'),
+                ]);
+            }
+        }
+    });
     }
 }
