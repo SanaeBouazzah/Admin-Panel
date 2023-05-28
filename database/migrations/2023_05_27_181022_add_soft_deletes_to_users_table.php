afc7fa891eae1198ad42a7125cf4ee94269f8446
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-28 20:27:29 +0200

    GV5-4

--- database/migrations/2023_05_27_181022_add_soft_deletes_to_users_table.php
+++ database/migrations/2023_05_27_181022_add_soft_deletes_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->softDeletes()->after('profile_photo_path');
+            $table->softDeletes();
         });
     }
 
