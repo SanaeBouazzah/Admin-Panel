32ab07b875602ce58931ead270c80871c6ac8864
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-28 22:00:49 +0200

    GV5-4

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
