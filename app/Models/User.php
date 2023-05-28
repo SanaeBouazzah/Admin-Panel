bd9f2e0c92de6dc554518a398646f7d7a64722b9
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-28 22:00:22 +0200

    GV5-4

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
 
