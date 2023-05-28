00069eb59e375e4c4992282f84305c5351b4cfee
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-28 21:27:37 +0200

    GV5-4

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -83,7 +83,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+          RolesRelationManager::class,
         ];
     }
     
