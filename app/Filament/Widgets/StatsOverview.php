c697c50e325a0ac9a1e3b6d933f9e5a13a8dbb92
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-03 03:12:33 +0200

    GV5-4

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            //
+        ];
+    }
+}
