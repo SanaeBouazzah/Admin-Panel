951f3fc12ccda739d2f5477e089aac4640335e58
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-03 03:26:28 +0200

    GV5-4

--- app/Filament/Widgets/UsersChart.php
+++ app/Filament/Widgets/UsersChart.php
@@ -2,27 +2,49 @@
 
 namespace App\Filament\Widgets;
 
+use Carbon\Carbon;
+use App\Models\User;
 use Filament\Widgets\BarChartWidget;
 
 class UsersChart extends BarChartWidget
 {
     protected static ?string $heading = 'Users';
 
-    protected function getData(): array
-    {
-      $users = User::select('created_at')->get()->groupby(function($users) [
-        return Carbon::parse($users->created_at)->format('F');
-      ]);
-      $quantities =[];
-      foreach
-      return [
-        'datasets' => [
-            [
-                'label' => 'Blog posts created',
-                'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+    protected function getData(): array {
+        $users = User::select('created_at')->get()->groupBy(function($users) {
+            return Carbon::parse($users->created_at)->format('F');
+        });
+        $quantities = [];
+        foreach ($users as $user => $value) {
+            array_push($quantities, $value->count());
+        }
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Users Joined',
+                    'data' => $quantities,
+                    'backgroundColor' => [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    'borderColor' => [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
+                    'borderWidth' => 1
+                ],
             ],
-        ],
-        'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    ];
+            'labels' => $users->keys(),
+        ];
     }
 }
