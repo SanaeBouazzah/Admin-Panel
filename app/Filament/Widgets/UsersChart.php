737320322fdfc7a6207ed3e4147547531a16f5cf
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-03 03:20:19 +0200

    GV5-4

--- app/Filament/Widgets/UsersChart.php
+++ app/Filament/Widgets/UsersChart.php
@@ -10,8 +10,14 @@ class UsersChart extends BarChartWidget
 
     protected function getData(): array
     {
-        return [
-            //
-        ];
+      return [
+        'datasets' => [
+            [
+                'label' => 'Blog posts created',
+                'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+            ],
+        ],
+        'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    ];
     }
 }
